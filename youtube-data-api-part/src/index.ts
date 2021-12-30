3d01616987d9fd9af18424c6ea30f1090501870f
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2021-12-31 01:34:44 +0900

    Impl sample code for youtube data api.

---
+++ youtube-data-api-part/src/index.ts
@@ -0,0 +1,26 @@
+import * as Dotenv from 'dotenv';
+import axios from 'axios';
+
+Dotenv.config();
+
+(async () => {
+  // 例: 「布袋寅泰 - バンビーナ / THE FIRST TAKE」のvideoIdの場合
+  const url = 'https://www.googleapis.com/youtube/v3/videos';
+  const videoId = 'G4zbSlU9XFY';
+
+  const response = await axios.get(url, {
+    method: 'GET',
+    params: {
+      id: videoId,
+      key: process.env.YOUTUBE_API_KEY,
+      part: 'snippet,statistics,status',
+      // fieldsを使うとレスポンスデータを絞ることができる（→レスポンスデータ量が減るため少し軽くなる）
+      // https://developers.google.com/youtube/v3/getting-started#fields
+      fields: 'items(id,snippet,statistics,status)',
+    },
+  });
+
+  const data = response.data;
+
+  console.log(JSON.stringify(data, null, 2));
+})();
