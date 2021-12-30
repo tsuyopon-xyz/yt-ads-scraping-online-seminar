e117a68a50754bd705243f1efdb445533735dbe7
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2021-12-31 02:10:35 +0900

    Created TasksModule

--- nestjs-part/src/app.module.ts
+++ nestjs-part/src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
-import { TasksService } from './tasks/tasks.service';
+import { TasksModule } from './tasks/tasks.module';
 
+// NestJSが用意しているサンプルコードと同じように
+// TasksModuleをimportsする形の実装にした。
+// https://github.com/nestjs/nest/blob/master/sample/27-scheduling/src/app.module.ts
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [ScheduleModule.forRoot(), TasksModule],
   controllers: [AppController],
-  providers: [AppService, TasksService],
+  providers: [AppService],
 })
 export class AppModule {}
