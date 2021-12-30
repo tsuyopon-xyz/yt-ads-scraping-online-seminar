3047d66621164f78fccd87ec9bdd37ae159fc56d
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2021-12-31 02:04:12 +0900

    Created tasks.service for  run program every 10 seconds.

--- nestjs-part/src/app.module.ts
+++ nestjs-part/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TasksService } from './tasks/tasks.service';
 
 @Module({
-  imports: [],
+  imports: [ScheduleModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TasksService],
 })
 export class AppModule {}
