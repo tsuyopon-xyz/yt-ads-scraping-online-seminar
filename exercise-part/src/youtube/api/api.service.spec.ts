4cdc4544332d7b7564d680f11b5db9ea6ec010cd
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-01-01 17:27:38 +0900

    Imple youtube video data api part.

---
+++ exercise-part/src/youtube/api/api.service.spec.ts
@@ -0,0 +1,32 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+import { ApiService } from './api.service';
+
+describe('ApiService', () => {
+  let service: ApiService;
+  const videoId = 'G4zbSlU9XFY';
+  const timeout = 1000 * 40;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
+      providers: [ApiService],
+    }).compile();
+
+    service = module.get<ApiService>(ApiService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it(
+    'YouTbe Data APIの動作確認用に作ったテスト（確認が済んだら "xit" でテストが実行されないようにする）',
+    async () => {
+      const videoData = await service.getVideo(videoId);
+
+      console.log({ videoData });
+    },
+    timeout,
+  );
+});
--- exercise-part/src/youtube/youtube.module.ts
+++ exercise-part/src/youtube/youtube.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ScrapingService } from './scraping/scraping.service';
+import { ApiService } from './api/api.service';
 
 @Module({
-  providers: [ScrapingService],
+  providers: [ScrapingService, ApiService],
 })
 export class YoutubeModule {}
